c4fce486e70fa455f5271ac6ce52e8c574f3ac00
Wael Antar <antarwael189@gmail.com>
2024-09-11 09:17:20 +0100

    1-Dependencies configuration + docker compose configuration

---
+++ SocialBooksAppBackend/src/main/java/com/projects/socialbooksappbackend/SocialBooksAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.projects.socialbooksappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialBooksAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialBooksAppBackendApplication.class, args);
+	}
+
+}
---
+++ SocialBooksAppBackend/src/test/java/com/projects/socialbooksappbackend/SocialBooksAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.socialbooksappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialBooksAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
