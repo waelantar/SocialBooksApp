5d113191a9d3dfd1e047780801d59f862782c77b
Wael Antar <antarwael189@gmail.com>
2024-09-30 23:26:54 +0100

    front-12 feat:Pagination completed

--- SocialBooksAppFrontend/src/app/modules/book/pages/book-list/book-list.component.ts
+++ SocialBooksAppFrontend/src/app/modules/book/pages/book-list/book-list.component.ts
@@ -14,26 +14,64 @@ import {BookCardComponent} from "../../components/book-card/book-card.component"
   styleUrl: './book-list.component.scss'
 })
 export class BookListComponent implements OnInit {
-  page=0;
-  size=5;
-  bookResponse:PageResponseBookResponse={};
-constructor(
-  private router:Router,
-  private bookService:BookService,
-) {
-}
-ngOnInit() {
-  this.findAllBooks();
-}
+  bookResponse: PageResponseBookResponse = {};
+  page = 0;
+  size = 5;
+  pages: any = [];
+  message = '';
+  level: 'success' |'error' = 'success';
+
+  constructor(
+    private bookService: BookService,
+    private router: Router
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.findAllBooks();
+  }
 
   private findAllBooks() {
-    this.bookService.findAllBooks(
-      {page:this.page,
-        size:this.size
-      }).subscribe({
-      next:(books)=> {
-this.bookResponse=books;
-      }
-      })
+    this.bookService.findAllBooks({
+      page: this.page,
+      size: this.size
+    })
+      .subscribe({
+        next: (books) => {
+          this.bookResponse = books;
+          this.pages = Array(this.bookResponse.totalPages)
+            .fill(0)
+            .map((x, i) => i);
+        }
+      });
+  }
+
+  gotToPage(page: number) {
+    this.page = page;
+    this.findAllBooks();
+  }
+
+  goToFirstPage() {
+    this.page = 0;
+    this.findAllBooks();
+  }
+
+  goToPreviousPage() {
+    this.page --;
+    this.findAllBooks();
+  }
+
+  goToLastPage() {
+    this.page = this.bookResponse.totalPages as number - 1;
+    this.findAllBooks();
+  }
+
+  goToNextPage() {
+    this.page++;
+    this.findAllBooks();
+  }
+
+  get isLastPage() {
+    return this.page === this.bookResponse.totalPages as number - 1;
   }
 }
